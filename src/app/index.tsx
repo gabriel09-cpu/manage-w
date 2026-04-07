afd4cbc30fee120b2131ddff09d3da20e0651d29
Gabriel <g.pedro0972@gmail.com>
2026-04-07 12:13:07 -0300

    Terminando Interface Gráfica

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,71 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View, Image } from "react-native";
 import { styles } from "./style";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 
 export default function Index() {
+  const [valor, setValor] = useState("");
   return (
     <View style={styles.container}>
-      
       {/* CABEÇALHO  */}
       <View style={styles.header}>
-        <Ionicons name="person"  size={32} color={"#fff"}/>
-      <Text style={styles.title}>Manage Workout</Text>
+        <Text style={styles.title}>Manage Workout</Text>
+        <TouchableOpacity>
+          <Ionicons name="person" size={32} color={"#fff"} />
+        </TouchableOpacity>
+      </View>
+
+      {/* MONTAGEM DO TREINO */}
+      <View style={styles.contents}>
+        <Text style={styles.titleContents}>Monte seu Treino:</Text>
+
+        <View style={styles.center}>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={valor}
+              onValueChange={(itemValue) => setValor(itemValue)}
+              dropdownIconColor="#fff"
+              style={styles.picker}
+            >
+              <Picker.Item label="Selecione..." value="" color="#999" />
+              <Picker.Item label="Peito" value="peito" />
+              <Picker.Item label="Costas" value="costas" />
+              <Picker.Item label="Perna" value="perna" />
+            </Picker>
+          </View>
+          <TouchableOpacity style={styles.searchButton}>
+            <Text style={styles.textButton}>Procurar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* RESPOSTA DA API  */}
+      <View style={styles.apiResponse}>
+
+        <Text>Supino Reto(Com barra)</Text>
+        <Image
+          style={{ width: 190, height: 190 }}
+          source={require("../assets/image/supinoreto.gif")}
+        />
+
+        <View style={styles.description}>
+          <Text>O supino reto é um exercício fundamental de 
+            musculação para o peitoral, realizado deitado em um
+            banco horizontal. Consiste em empurrar uma barra ou 
+            halteres para cima, partindo da altura do peito, 
+            focado no desenvolvimento do peitoral maior, tríceps e 
+            deltoides anteriores, 
+            mantendo os pés firmes no chão e escápulas travadas</Text>
+        </View>
+        <TouchableOpacity style={styles.guardButton}>
+          <Text>Guardar</Text>
+        </TouchableOpacity>
+
+          <TouchableOpacity style={styles.ignoreButtons}>
+          <Text>Ignorar</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- src/app/style.tsx
+++ src/app/style.tsx
@@ -1,25 +1,89 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#2b2b2b"
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#2b2b2b",
+  },
 
-    header: {
-        paddingHorizontal: 15,
-        paddingTop: 20,
-        paddingBottom: 20,
-        flexDirection: 'row',
-        alignItems: "center",
-        gap: 10, // espaço entre ícone e texto
-        backgroundColor: "#49494a",
-    },
+  header: {
+    paddingHorizontal: 15,
+    paddingTop: 20,
+    paddingBottom: 20,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    gap: 100, // espaço entre ícone e texto
+    backgroundColor: "#49494a",
+  },
 
-    title: {
-        fontSize: 24,
-        color: "#e5e5e5",
-        fontFamily: "GoogleSans-Bold"
-    },
+  title: {
+    fontSize: 24,
+    color: "#e5e5e5",
+    fontFamily: "GoogleSans-Bold",
+  },
 
-})
+  content: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    alignItems: "center",
+  },
+
+  contents: {
+    padding: 15,
+  },
+
+  titleContents: {
+    fontSize: 18,
+    color: "#e5e5e5",
+    marginBottom: 10,
+    fontFamily: "GoogleSans-Bold",
+    textAlign: "center", // opcional
+  },
+
+  center: {
+    alignItems: "center", // 🔥 centraliza horizontalmente
+  },
+
+  pickerContainer: {
+    width: "70%", // 🔥 controla o tamanho (pode testar 60%, 50%)
+    backgroundColor: "#3a3a3a",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#555",
+    overflow: "hidden",
+  },
+
+  picker: {
+    color: "#fff",
+  },
+
+  searchButton: {
+    width: 200,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#e5e5e5",
+    borderRadius:10,
+    marginTop: 18,
+  },
+
+  textButton: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: "GoogleSans-Bold",
+  }, 
+
+  apiResponse: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#404040",
+    width: 340,
+    height: 390,
+    borderRadius: 16,
+    alignSelf: 'center'
+
+   }
+});
